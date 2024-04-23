fd79e7d6ff89a4164c326848900d0769f6203a5b
phanbaokhang0205 <phanbaokhang0205@gmail.com>
2024-04-23 22:03:18 +0700

    update UI

--- MuisicApp/app/src/main/java/com/example/muisicapp/View/Home/HomeScreen.kt
+++ MuisicApp/app/src/main/java/com/example/muisicapp/View/Home/HomeScreen.kt
@@ -1,29 +1,51 @@
 package com.example.muisicapp.View.Home
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.NavigateNext
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
-import coil.compose.rememberAsyncImagePainter
-import coil.compose.rememberImagePainter
 import com.example.muisicapp.Model.data.Song
 import com.example.muisicapp.ViewModel.AppViewModelProvider
 import com.example.muisicapp.ViewModel.HomeViewModel
 import com.example.muisicapp.ui.theme.MuisicAppTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     viewModel: HomeViewModel = viewModel(factory = AppViewModelProvider.Factory)
@@ -31,16 +53,49 @@ fun HomeScreen(
 
     val homeUiState by viewModel.homeUiState.collectAsState()
 
-    Scaffold(
 
-    ) { innerPadding ->
+    var isFavourite by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    var isPlay by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.Black,
+                    titleContentColor = Color.White,
+                ),
+                title = {
+                    ContentTopAppBar()
+                }
+            )
+        },
+        bottomBar = {
+            BottomAppBar(
+                onClickFavourite = {
+                    isFavourite = !isFavourite
+                },
+                onClickPlaying = {
+                    isPlay = !isPlay
+                },
+                isFavourite = isFavourite,
+                isPlaying = isPlay
+            )
+        },
+
+        ) { innerPadding ->
+
+        NavigationTitle("Songs for you", {})
+
         HomeBody(
             songList = homeUiState.songList,
             contentPadding = innerPadding
         )
     }
-
-
 }
 
 @Composable
@@ -48,10 +103,16 @@ private fun HomeBody(
     songList: List<Song>,
     contentPadding: PaddingValues = PaddingValues(0.dp)
 ) {
-    Column {
+
+    Column(
+        modifier = Modifier.background(Color.Black)
+    ) {
+
+
         if (songList.isEmpty()) {
             Text(text = "The list is empty")
         } else {
+
             SongList(
                 songList,
                 modifier = Modifier,
@@ -68,9 +129,10 @@ private fun SongList(
     contentPadding: PaddingValues,
     modifier: Modifier = Modifier,
 ) {
-    LazyColumn(
+    LazyRow(
         modifier = modifier,
-        contentPadding = contentPadding
+        contentPadding = contentPadding,
+        horizontalArrangement = Arrangement.spacedBy(20.dp)
     ) {
         items(items = songList, key = { it.songId!! }) { song ->
             SongItem(song = song)
@@ -80,18 +142,72 @@ private fun SongList(
 
 @Composable
 private fun SongItem(
-    song: Song
+    song: Song,
 ) {
-
     Column(
         modifier = Modifier
+            .background(Color.Black)
+            .width(130.dp)
     ) {
-        AsyncImage(
-            model = song.songImage,
-            contentDescription = null,
-            modifier = Modifier.size(300.dp)
-        )
-        Text(text = song.songName)
+
+        Box(
+            modifier = Modifier
+                .size(130.dp)
+                .clip(RoundedCornerShape(16.dp))
+                .background(Color.Black)
+        ) {
+            AsyncImage(
+                model = song.songImage,
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.background(Color.Black)
+            )
+        }
+        Spacer(modifier = Modifier.height(12.dp))
+        Text(text = song.songName, fontSize = 12.sp, color = Color.White)
+
+        Text(text = song.songName, fontSize = 10.sp, color = Color(0xff808080))
+    }
+}
+
+@Composable
+fun NavigationTitle(
+    navTitle: String,
+    onClickNavigation: () -> Unit,
+) {
+    Row(
+        modifier = Modifier
+            .background(Color.Black)
+            .fillMaxWidth()
+    ) {
+        TextButton(onClick = {
+            onClickNavigation()
+        }) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = navTitle,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White
+                )
+                Icon(
+                    imageVector = Icons.Filled.NavigateNext,
+                    contentDescription = null,
+                    tint = Color.White
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TextButtonPreview() {
+    MuisicAppTheme {
+//        NavigationTitle()
     }
 }
 
@@ -105,7 +221,7 @@ fun HomeBodyPreview() {
                     1,
                     "Yêu anh hơn chính em",
                     "image",
-                    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.last.fm%2Fmusic%2FSon%2BTung%2BM-TP%2F%2Bimages&psig=AOvVaw2iLQ7Ex40A4KyftUAhAJee&ust=1713946154654000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCIiZqJDx14UDFQAAAAAdAAAAABAK",
+                    "",
                     3
                 ),
                 Song(2, "Chúng ta của hiện tại", "image", "link", 1),
